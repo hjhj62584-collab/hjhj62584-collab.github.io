b32ed350d843ac7d2e0961da5ddb57d2add41a38
hjhj62584-collab <hjhj62584@gmail.com>
2026-02-16 19:38:00 +0300

    test

--- index.tsx
+++ index.tsx
@@ -1,13 +1,10 @@
 
-
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideZonelessChangeDetection } from '@angular/core';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { AppComponent } from './src/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideZonelessChangeDetection()
+    provideExperimentalZonelessChangeDetection()
   ]
 }).catch(err => console.error(err));
-
-// AI Studio always uses an `index.tsx` file for all project types.
